3ed2a621ab2e8b79db5a6d72f09c5834e42afe69
Peter Vandenberk <pvandenberk@gmail.com>
2010-12-18 21:12:34 +0000

    count the number of read, processed and ignored lines, and print out summary after processing (when using the new -v/--verbose option)

--- lib/ntail/application.rb
+++ lib/ntail/application.rb
@@ -10,8 +10,14 @@ module NginxTail
     end
     
     def self.ntail_options
+      # shamelessly copied from lib/rake.rb (rake gem)
       [
-        ['-v', '--version', '-V', "Display the program version.",
+        ['--verbose', '--v', "Run verbosely (log messages to STDERR).",
+          lambda { |value|
+            self.options.verbose = true
+          }
+        ],
+        ['--version', '-V', "Display the program version.",
           lambda { |value|
             puts "#{NTAIL_NAME}, version #{NTAIL_VERSION}"
             self.options.exit = true
@@ -48,16 +54,26 @@ module NginxTail
     def self.run!
       self.parse_options
       unless self.options.exit
+        lines_read = lines_processed = lines_ignored = 0
         ARGF.each_line do |line|
-          line = line.chomp
-          if !self.options.filter or self.options.filter.call line
+          line = line.chomp ; lines_read += 1
+          if !self.options.filter || self.options.filter.call line
+            lines_processed += 1
             if self.options.code
               self.options.code.call line
             else
               puts line
             end
+          else
+            lines_ignored += 1
+            if self.options.verbose
+              $stderr.puts "[WARN] ignoring line ##{lines_read}"
+            end
           end
         end
+      end
+      if self.options.verbose
+        $stderr.puts "[INFO] read #{lines_read} lines, processed #{lines_processed} lines, ignored #{lines_ignored} lines"
       end
       return 0
     end # def run
