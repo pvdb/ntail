ebae8db3cf18a225f4d3be490af531fd7b922d82
Peter Vandenberk <pvandenberk@gmail.com>
2010-12-18 21:31:55 +0000

    added some basic interrupt handling

--- lib/ntail/application.rb
+++ lib/ntail/application.rb
@@ -20,7 +20,7 @@ module NginxTail
         ['--version', '-V', "Display the program version.",
           lambda { |value|
             puts "#{NTAIL_NAME}, version #{NTAIL_VERSION}"
-            self.options.exit = true
+            self.options.running = false
           }
         ],
         ['--filter',  '-f CODE', "Ruby code block for filtering (parsed) lines - needs to return true or false.",
@@ -37,6 +37,10 @@ module NginxTail
     end
 
     def self.parse_options
+      
+      # application defaults...
+      self.options.running = true
+      
       OptionParser.new do |opts|
         opts.banner = "ntail {options} {file(s)} ..."
         opts.separator ""
@@ -44,7 +48,7 @@ module NginxTail
 
         opts.on_tail("-h", "--help", "-H", "Display this help message.") do
           puts opts
-          self.options.exit = true
+          self.options.running = false
         end
 
         self.ntail_options.each { |args| opts.on(*args) }
@@ -52,30 +56,40 @@ module NginxTail
     end
     
     def self.run!
+      
       self.parse_options
-      unless self.options.exit
-        lines_read = lines_processed = lines_ignored = 0
-        ARGF.each_line do |line|
-          line = line.chomp ; lines_read += 1
-          if !self.options.filter || self.options.filter.call(line)
-            lines_processed += 1
-            if self.options.code
-              self.options.code.call line
-            else
-              puts line
-            end
+      
+      ['TERM', 'INT'].each do |signal|
+        Signal.trap(signal) do
+          self.options.running = false ; puts
+        end
+      end
+      
+      lines_read = lines_processed = lines_ignored = 0
+      
+      while self.options.running and ARGF.gets
+        line = $_.chomp ; lines_read += 1
+        if !self.options.filter || self.options.filter.call(line)
+          lines_processed += 1
+          if self.options.code
+            self.options.code.call line
           else
-            lines_ignored += 1
-            if self.options.verbose
-              $stderr.puts "[WARN] ignoring line ##{lines_read}"
-            end
+            puts line
+          end
+        else
+          lines_ignored += 1
+          if self.options.verbose
+            $stderr.puts "[WARN] ignoring line ##{lines_read}"
           end
         end
       end
+      
       if self.options.verbose
         $stderr.puts "[INFO] read #{lines_read} lines, processed #{lines_processed} lines, ignored #{lines_ignored} lines"
       end
+      
       return 0
+      
     end # def run
     
   end
