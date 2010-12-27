bf7580f40100b84b65c91a05abcb4cc2c3324fc0
Peter Vandenberk <pvandenberk@mac.com>
2010-12-27 12:36:28 +0000

    test date conversion on actual log lines, incl. new factory method for raw and parsed log lines

--- test/helper.rb
+++ test/helper.rb
@@ -25,4 +25,47 @@ class Test::Unit::TestCase
     (['192', '168'] + (1..2).map { Kernel.rand(256) }).join('.')
   end
 
+  # 
+  # http://wiki.nginx.org/NginxHttpLogModule#log_format
+  #
+  # There is a predefined log format called "combined":
+  # 
+  # log_format combined '$remote_addr - $remote_user [$time_local] '
+  #                     '"$request" $status $body_bytes_sent '
+  #                     '"$http_referer" "$http_user_agent"';
+  # 
+
+  LOG_FORMAT_COMBINED = '%s - %s [%s] ' \
+                        '"%s" %d %d ' \
+                        '"%s" "%s"'
+                        
+  DEFAULT_VALUES = {
+    :remote_addr => '72.46.130.42',
+    :remote_user => '-',
+    :time_local => '01/Jan/2010:04:00:29 +0000',
+    :request => 'GET /index.html HTTP/1.1',
+    :status => 200,
+    :body_bytes_sent => 6918,
+    :http_referer => 'http://www.google.com/search?q=example',
+    :http_user_agent => 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.224 Safari/534.10'
+  }
+
+  def random_raw_line(options = {})
+    options = DEFAULT_VALUES.merge options
+    LOG_FORMAT_COMBINED % [
+      options[:remote_addr],
+      options[:remote_user],
+      options[:time_local],
+      options[:request],
+      options[:status],
+      options[:body_bytes_sent],
+      options[:http_referer],
+      options[:http_user_agent],
+    ]
+  end
+  
+  def random_log_line(options = {})
+    NginxTail::LogLine.new(random_raw_line(options))
+  end
+  
 end
--- test/ntail/test_variable_conversions.rb
+++ test/ntail/test_variable_conversions.rb
@@ -2,16 +2,22 @@ require 'helper'
 
 class TestVariableConversions < Test::Unit::TestCase
 
-  should "parse local time (in the common log format) into a date" do
+  should "parse local time (in the common log format) into a date object" do
     time_local = "13/Apr/2010:04:45:51 +0100"
     to_date = NginxTail::LogLine.to_date(time_local)
     assert_equal "2010-04-13T04:45:51+01:00", to_date.to_s
+
+    log_line = random_log_line(:time_local => '19/Dec/2010:05:01:29 +0000')
+    assert_equal "2010-12-19T05:01:29+00:00", log_line.to_date.to_s
   end
 
   should "parse and format local time (in the common log format) into a date string" do
     time_local = "13/Apr/2010:04:45:51 +0100"
     to_date_s = NginxTail::LogLine.to_date_s(time_local)
     assert_equal "2010-04-13 04:45:51", to_date_s
+
+    log_line = random_log_line(:time_local => '16/Jan/2010:04:00:29 +0000')
+    assert_equal "2010-01-16 04:00:29", log_line.to_date_s    
   end
 
 end
