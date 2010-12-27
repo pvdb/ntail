aa8e944c27e1ff98dad1c1a4817fe8574ad6ad6e
Peter Vandenberk <pvandenberk@mac.com>
2010-12-27 10:42:30 +0000

    whitespace, to make rcov reports correct/more meaningful (no functional changes)

--- lib/ntail/http_referers.rb
+++ lib/ntail/http_referers.rb
@@ -13,13 +13,30 @@ module NginxTail
         @@internal_referers = []
 
         # mainly (solely?) for testing purposes...
-        def self.reset_internal_referers() while !@@internal_referers.empty? ; @@internal_referers.pop ; end ; end
-        def self.internal_referers() @@internal_referers.dup ; end
-        def self.add_internal_referer(internal_referer) (@@internal_referers << internal_referer).uniq! ; end
+        def self.reset_internal_referers()
+          while !@@internal_referers.empty? ; @@internal_referers.pop ; end
+        end
+        
+        # mainly (solely?) for testing purposes...
+        def self.internal_referers()
+          @@internal_referers.dup
+        end
+        
+        def self.add_internal_referer(internal_referer)
+          (@@internal_referers << internal_referer).uniq!
+        end
 
-        def self.unknown_referer?(http_referer)  http_referer == "-" ; end
-        def self.internal_referer?(http_referer) !self.unknown_referer?(http_referer) && !@@internal_referers.detect { |referer| referer.match(http_referer) }.nil? ; end
-        def self.external_referer?(http_referer) !self.unknown_referer?(http_referer) && !self.internal_referer?(http_referer) ; end
+        def self.unknown_referer?(http_referer)
+          http_referer == "-"
+        end
+        
+        def self.internal_referer?(http_referer)
+          !self.unknown_referer?(http_referer) && !@@internal_referers.detect { |referer| referer.match(http_referer) }.nil?
+        end
+        
+        def self.external_referer?(http_referer)
+          !self.unknown_referer?(http_referer) && !self.internal_referer?(http_referer)
+        end
 
       end
     end
