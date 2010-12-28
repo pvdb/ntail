7df0ae8376b95f7f2732c6f98d34a3cb405bb7be
Peter Vandenberk <pvandenberk@mac.com>
2010-12-28 10:29:52 +0000

    use the shoulda context helpers (no functional changes)

--- test/ntail/test_time_local.rb
+++ test/ntail/test_time_local.rb
@@ -2,22 +2,29 @@ require 'helper'
 
 class TestTimeLocal < Test::Unit::TestCase
 
-  should "parse local time (in the common log format) into a date object" do
-    time_local = "13/Apr/2010:04:45:51 +0100"
-    to_date = NginxTail::LogLine.to_date(time_local)
-    assert_equal "2010-04-13T04:45:51+01:00", to_date.to_s
+  context "ntail" do
+    
+    setup do
+      @time_local = "13/Apr/2010:04:45:51 +0100"
+      @log_line = random_log_line(:time_local => @time_local)
+    end
 
-    log_line = random_log_line(:time_local => '19/Dec/2010:05:01:29 +0000')
-    assert_equal "2010-12-19T05:01:29+00:00", log_line.to_date.to_s
-  end
+    should "parse local time (in the common log format) into a date object" do
+      # directly via the helper function
+      to_date = NginxTail::LogLine.to_date(@time_local)
+      assert_equal "2010-04-13T04:45:51+01:00", to_date.to_s
+      # parsed from a raw log line
+      assert_equal "2010-04-13T04:45:51+01:00", @log_line.to_date.to_s
+    end
 
-  should "parse and format local time (in the common log format) into a date string" do
-    time_local = "13/Apr/2010:04:45:51 +0100"
-    to_date_s = NginxTail::LogLine.to_date_s(time_local)
-    assert_equal "2010-04-13 04:45:51", to_date_s
-
-    log_line = random_log_line(:time_local => '16/Jan/2010:04:00:29 +0000')
-    assert_equal "2010-01-16 04:00:29", log_line.to_date_s    
+    should "parse and format local time (in the common log format) into a date string" do
+      # directly via the helper function
+      to_date_s = NginxTail::LogLine.to_date_s(@time_local)
+      assert_equal "2010-04-13 04:45:51", to_date_s
+      # parsed from a raw log line
+      assert_equal "2010-04-13 04:45:51", @log_line.to_date_s    
+    end
+    
   end
 
 end
