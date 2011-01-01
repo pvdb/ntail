f47d9e0474e2e2c913bd0abfcd95a853f6a7caab
Peter Vandenberk <pvandenberk@mac.com>
2011-01-01 11:39:26 +0000

    ability to process a single line with a specific line number (mainly for debugging purposes)

--- lib/ntail/application.rb
+++ lib/ntail/application.rb
@@ -12,12 +12,12 @@ module NginxTail
     def self.ntail_options
       # shamelessly copied from lib/rake.rb (rake gem)
       [
-        ['--verbose', '--v', "Run verbosely (log messages to STDERR).",
+        ['--verbose', '-v', "Run verbosely (log messages to STDERR).",
           lambda { |value|
             self.options.verbose = true
           }
         ],
-        ['--dry-run', '--n', "Dry-run: process files, but don't actually parse the lines",
+        ['--dry-run', '-n', "Dry-run: process files, but don't actually parse the lines",
           lambda { |value|
             self.options.dry_run = true
           }
@@ -26,6 +26,11 @@ module NginxTail
           lambda { |value|
             puts "#{NTAIL_NAME}, version #{NTAIL_VERSION}"
             self.options.running = false
+          }
+        ],
+        ['--line-number', '-l LINE_NUMBER', "Only process the line with the given line number",
+          lambda { |value|
+            self.options.line_number = value.to_i
           }
         ],
         ['--filter',  '-f CODE', "Ruby code block for filtering (parsed) lines - needs to return true or false.",
@@ -82,35 +87,37 @@ module NginxTail
         end
         raw_line = $_.chomp ; lines_read += 1
         unless self.options.dry_run
-          begin
-            log_line = NginxTail::LogLine.new(raw_line)
-            if log_line.parsable
-              parsable_lines += 1
-              if !self.options.filter || self.options.filter.call(log_line)
-                lines_processed += 1
-                if self.options.code
-                  self.options.code.call(log_line)
+          if !self.options.line_number or self.options.line_number == ARGF.lineno
+            begin
+              log_line = NginxTail::LogLine.new(raw_line)
+              if log_line.parsable
+                parsable_lines += 1
+                if !self.options.filter || self.options.filter.call(log_line)
+                  lines_processed += 1
+                  if self.options.code
+                    self.options.code.call(log_line)
+                  else
+                    puts log_line
+                  end
                 else
-                  puts log_line
+                  lines_ignored += 1
+                  if self.options.verbose
+                    $stderr.puts "[WARNING] ignoring line ##{lines_read}"
+                  end
                 end
               else
-                lines_ignored += 1
+                unparsable_lines += 1
                 if self.options.verbose
-                  $stderr.puts "[WARNING] ignoring line ##{lines_read}"
+                  $stderr.puts "[ERROR] cannot parse '#{raw_line}'"
                 end
               end
-            else
-              unparsable_lines += 1
-              if self.options.verbose
-                $stderr.puts "[ERROR] cannot parse '#{raw_line}'"
-              end
+            rescue
+              $stderr.puts "[ERROR] processing line #{ARGF.file.lineno} of file #{ARGF.filename} resulted in #{$!.message}"
+              $stderr.puts "[ERROR] " + raw_line
+              self.options.exit = -1
+              self.options.running = false
+              raise $! # TODO if the "re-raise exceptions" option has been set...
             end
-          rescue
-            $stderr.puts "[ERROR] processing line #{lines_read} of file #{ARGF.filename} resulted in #{$!.message}"
-            $stderr.puts "[ERROR] " + raw_line
-            self.options.exit = -1
-            self.options.running = false
-            raise $! # TODO if the "re-raise exceptions" option has been set...
           end
         end
       end
