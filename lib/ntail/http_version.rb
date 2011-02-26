890d393940bc9088545f76ef0343e9401a5e5e48
Peter Vandenberk <pvandenberk@mac.com>
2011-02-26 11:27:00 +0000

    refactoring of http version component, incl. unit tests

--- lib/ntail/http_version.rb
+++ lib/ntail/http_version.rb
@@ -4,8 +4,23 @@ module NginxTail
     def self.included(base) # :nodoc:
       base.class_eval do
 
-        def self.to_http_version_s(http_version)
-          http_version || "" # will be nil if $request == "-" (ie. "dodgy" HTTP requests)
+        @@http_version_expression = Regexp.compile('HTTP/([0-9])\.([0-9])')
+
+        def self.minor_version(http_version)
+          return $1 if http_version =~ @@http_version_expression
+        end
+
+        def self.major_version(http_version)
+          return $1 if http_version =~ @@http_version_expression
+        end
+
+        def self.to_http_version_s(http_version, which = :full)
+          # http_version will be nil if $request == "-" (ie. "dodgy" HTTP requests)
+          http_version.nil? ? "" : case which
+            when :full then http_version
+            when :major then self.major_version(http_version)
+            when :minor then self.minor_version(http_version)
+          end
         end
 
         # this ensures the below module methods actually make sense...
@@ -14,8 +29,8 @@ module NginxTail
       end
     end
 
-    def to_http_version_s
-      self.class.to_http_version_s(self.http_version)
+    def to_http_version_s(which = :full)
+      self.class.to_http_version_s(self.http_version, which)
     end
     
   end
